820c0b60be3a8a355e91a084ddb593abc595b3f8
DerekAyala <derek.ayala12345@hotmail.com>
2021-12-08 17:32:17 -0600

    new Funtions Added
    -The Quality of an item is never negative
    -The Quality of an item is never more than 50
    -"Conjured" items degrade in Quality twice as fast as normal items
    
    Co-authored-by: alanRazoMen <arazo@ucol.mx>
    Co-authored-by: IrenehMDev <ireneh.dev_03@icloud.com>

--- app/gilded_rose.rb
+++ app/gilded_rose.rb
@@ -8,28 +8,63 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-      elsif item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
-          item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
-        end
-      end
+      item_name(item)
       item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
-      if item.sell_in.negative?
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-          else
-            item.quality -= item.quality
-          end
-        elsif item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
+      aged_brie(item) if item.sell_in.negative?
+      conjured(item)
+      quality_rules(item)
     end
+  end
+
+  def item_name(item)
+    if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+      item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+    elsif item.quality < 50
+      item.quality = item.quality + 1
+      backstage_passes(item)
+    end
+  end
+
+  def backstage_passes(item)
+    return unless item.name == 'Backstage passes to a TAFKAL80ETC concert'
+
+    return unless item.sell_in < 11 && (item.quality < 50)
+
+    item.quality = item.quality + 1
+    return unless item.sell_in < 6 && (item.quality < 50)
+
+    item.quality = item.quality + 1
+  end
+
+  def aged_brie(item)
+    if item.name != 'Aged Brie'
+      quality(item)
+    elsif item.quality < 50
+      item.quality = item.quality + 1
+    end
+  end
+
+  def quality(item)
+    if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+    else
+      item.quality -= item.quality
+    end
+  end
+
+  def conjured(item)
+    return unless item.name == 'Conjured'
+
+    item.quality = item.quality - 1
+
+    return unless item.sell_in.negative?
+
+    item.quality = item.quality - 1
+  end
+
+  def quality_rules(item)
+    item.quality = 50 if item.quality > 50
+    item.quality = 0 if item.quality.negative?
   end
 end
 
@@ -47,3 +82,7 @@ class Item
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
+
+items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 8, 10)]
+GildedRose.new(items).update_quality
+puts(items)
